27df5d2710a0354673021a8677b8c99185809d6b
vitor <vitorcarvalhosa12@gmail.com>
2025-10-01 19:33:15 -0300

    migrate feito e app nas configs

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'athena.apps.AthenaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
