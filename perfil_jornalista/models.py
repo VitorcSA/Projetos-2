e5a77b1c60d2c6a4c94003ff2637d404a065311e
Gabriel Nunes Martins <gabrielnnsmartins@hotmail.com>
2025-11-05 18:44:53 -0300

    Ajustes no último commit e adicionar mais algumas funções de edição de texto

--- perfil_jornalista/models.py
+++ perfil_jornalista/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.contrib.auth import get_user_model
 # Create your models here.
 
+User = get_user_model()
+
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.Charfield(max_lenght=50)
     conteudo = RichTextField()
     data_postagem = models.DateTimeField(auto_now_add=True)
@@ -10,3 +14,5 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-data atualizada"]
+
+
--- perfil_jornalista/views.py
+++ perfil_jornalista/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
-from .forms import PostCreateform
+from .forms import PostCreateform, UpdatePostForm
 
 # Create your views here.
+@login_required
 def editor_texto(request):
     if request.method == "POST":
-        form = CriarPost(request.POST)
+        form = PostCreateform(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
         
         else:
-            form = CriarPost()
+            form = PostCreateform()
         #Quando estiver com o frontend pronto coloca dentro do parâmetro do render!
         return render()
+    
+class atualizar_texto(request):
+    model = Post
+    form_class = UpdatePostForm
+    template_name = ""
+    success_message = "Matéria Atualizada!"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.user:
+            return True
+        return False
